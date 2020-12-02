972cbe76689545dbfa05546b2516be3e7f842702
Ev <29648607+evanesoteric@users.noreply.github.com>
2020-12-01 22:31:42 -0500

    Update install-firefox.sh

--- install-firefox.sh
+++ install-firefox.sh
@@ -9,9 +9,9 @@ version="81.0.2"
 cd /tmp
 
 # install firefox
-wget https://ftp.mozilla.org/pub/firefox/releases/${fversion}/linux-x86_64/en-US/firefox-${fversion}.tar.bz2
-tar -xf firefox-${fversion}.tar.bz2
+wget https://ftp.mozilla.org/pub/firefox/releases/${version}/linux-x86_64/en-US/firefox-${version}.tar.bz2
+tar -xf firefox-${version}.tar.bz2
 chown root:root -R firefox
 mv firefox/ /opt
 ln -f -s /opt/firefox/firefox /usr/bin/firefox
-rm firefox-${fversion}.tar.bz2
+rm firefox-${version}.tar.bz2
